573efd2a2f9a7cc1a352651e9b60f6aa9cd7ca51
develop.lp <develop.lp@gmail.com>
2011-11-14 03:03:46 +0000

    Primera subversión, loging a SQL Server, con SA. Visual Studio 2010.

---
+++ Proyecto/Manejadro_Base_de_Datos/Manejadro_Base_de_Datos/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Manejadro_Base_de_Datos
+{
+    class Usuario
+    {
+        //Datos de la clase
+        public enum enumTipo
+        {
+            SQLServer = 1,
+            MySQL = 2,
+            SQLite = 3,
+            Access = 4
+        };
+        public static String strConeccion;
+        private SqlConnection sqlServerConnection;
+        private SqlCommand sqlServerCommand;
+
+        //Constructor de la clase.
+        public Usuario(String strUsuario, String strPassword,int TipoBD)
+        {
+            if (TipoBD == (int)enumTipo.SQLServer)
+            {
+                strConeccion = "Data Source = localhost; User Id = "+strUsuario+"; Password="+strPassword+";";                
+                sqlServerConnection = new SqlConnection(strConeccion);
+                sqlServerConnection.Open();
+            }
+        }
+
+    }
+}
